ff0c4079430aafe24e9b743d76a707b86ea08949
Josh <joshds313@gmail.com>
2022-05-10 16:35:16 -0400

    here we go again

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
-import postcssImport from 'postcss-import';
 import dts from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
@@ -22,19 +21,12 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss({
-        plugins: [postcssImport()],
-      }),
-    ],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.(css|scss)$/],
+    external: [/\.(css|less|scss)$/],
   },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+import './components/common/_themes.scss';
 export * from './components';
