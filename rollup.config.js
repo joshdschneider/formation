320cd4326dd82a368ae0203045238827e326d0ff
Josh <joshds313@gmail.com>
2022-05-10 12:59:35 -0400

    package fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,35 +2,31 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
 
-const config = [
+export default [
   {
     input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
         format: 'cjs',
+        sourcemap: true,
       },
       {
         file: packageJson.module,
         format: 'esm',
+        sourcemap: true,
       },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: './tsconfig.json',
-      }),
-      postcss({
-        extract: true,
-        minimize: true,
-        use: ['sass'],
-      }),
-    ],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
-
-export default config;
