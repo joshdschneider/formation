35651dde4116a9623b5f96685f9f9cad4778d104
Josh <joshds313@gmail.com>
2022-06-02 16:47:51 -0400

    added native props for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,17 +5,7 @@ import './_button.scss';
 export class Button extends AbstractButton {
   render() {
     return (
-      <button
-        type={this.props.type}
-        name={this.props.name}
-        value={this.props.value}
-        id={this.props.id}
-        className={this.getClasses()}
-        style={this.props.style}
-        onClick={this.props.onClick}
-        ref={this.props.ref}
-        disabled={this.getDisabled()}
-      >
+      <button className={this.getClasses()} disabled={this.getDisabled()}>
         {this.getChildren()}
       </button>
     );
